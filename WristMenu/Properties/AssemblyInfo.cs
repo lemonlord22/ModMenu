1643f5ede24b07c5cbfa7c734ab5fa161109d3d9
logundev <102193250+logundev@users.noreply.github.com>
2022-04-11 12:47:46 -0400

    Update AssemblyInfo.cs

--- WristMenu/Properties/AssemblyInfo.cs
+++ WristMenu/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WristMenu")]
+[assembly: AssemblyTitle("Mango Menu")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WristMenu")]
+[assembly: AssemblyProduct("Mango Menu")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
